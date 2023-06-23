00a9475c4ffd70bf8f6249a95907a94f57ae59cd
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-23 21:52:16 +0530

    [Ritvik]Add: Welcome Message

---
+++ MSTestPracticeProblems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MSTestPracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ MSTestPracticeProblems/ShopKeeper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTestPracticeProblems
+{
+    public class ShopKeeper
+    {
+        public int SoldOnceItem(List<int> prices)
+        {
+            Dictionary<int,int> counter = new Dictionary<int,int>();
+
+            foreach(int price in prices)
+            {
+                if (counter.ContainsKey(price))
+                {
+                    counter[price]++;
+                }
+                else
+                {
+                    counter[price] = 1;
+                }
+            }
+
+            foreach(int price in prices)
+            {
+                if (counter[price] == 1)
+                {
+                    return price;
+                }
+            }
+            return default;
+        }
+    }
+}
